7e80911902781f187fa62e563d277c4632a8cad3
irsyadul_ibat <irsyadulibat24@gmail.com>
2021-06-13 16:37:24 +0700

    marketplace_bangdulstore

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,7 @@
+ <script src="/vendor/jquery/jquery.slim.min.js"></script>  <!-- versi slim -->
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> <!-- bootstrap yang memerlukan js -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init(); 
+    </script>
+    <script src="/script/navbar-scroll.js"></script>
---
+++ routes/web.php
@@ -0,0 +1,86 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/categories', 'CategoryController@index')->name('categories');
+Route::get('/categories/{id}', 'CategoryController@detail')->name('categories-detail');
+
+Route::get('/details/{id}', 'DetailController@index')->name('detail');
+Route::post('/details/{id}', 'DetailController@add')->name('detail-add');
+
+
+
+
+Route::get('/checkout/callback', 'CheckoutController@callback')->name('midtrans-callback');
+
+Route::get('/success', 'CartController@success')->name('success');
+
+Route::get('/register/success', 'Auth\RegisterController@success')->name('register-success');
+
+
+
+Route::group(['middleware'=> ['auth']], function(){
+        Route::get('/cart', 'CartController@index')->name('cart');
+        Route::delete('/cart/{id}', 'CartController@delete')->name('cart-delete');
+        Route::post('/checkout', 'CheckoutController@proccess')->name('checkout');
+
+        Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+
+        Route::get('/dashboard/products', 'DashboardProductController@index')
+            ->name('dashboard-products');
+        Route::get('/dashboard/products/create', 'DashboardProductController@create')
+            ->name('dashboard-products-create');
+        Route::post('/dashboard/products', 'DashboardProductController@store')
+            ->name('dashboard-products-store');
+        Route::get('/dashboard/products/{id}', 'DashboardProductController@details')
+            ->name('dashboard-products-details');
+        Route::post('/dashboard/products/{id}', 'DashboardProductController@update')
+            ->name('dashboard-products-update');
+
+        Route::post('/dashboard/products/gallery/upload', 'DashboardProductController@uploadGallery')
+            ->name('dashboard-products-gallery-upload');
+        Route::get('/dashboard/products/gallery/delete/{id}', 'DashboardProductController@deleteGallery')
+            ->name('dashboard-products-gallery-delete');
+
+        Route::get('/dashboard/transactions', 'DashboardTransactionController@index')
+            ->name('dashboard-transactions');
+        Route::get('/dashboard/transactions/{id}', 'DashboardTransactionController@details')
+            ->name('dashboard-transactions-details');
+        Route::post('/dashboard/transactions/{id}', 'DashboardTransactionController@update')
+            ->name('dashboard-transactions-update');
+
+        Route::get('/dashboard/settings', 'DashboardSettingController@store')
+            ->name('dashboard-settings-store');
+        Route::get('/dashboard/account', 'DashboardSettingController@account')
+            ->name('dashboard-settings-account');
+        Route::post('/dashboard/account/{redirect}', 'DashboardSettingController@update')
+            ->name('dashboard-settings-redirect');
+    });
+
+    //->middleware(['auth','admin'])
+Route::prefix('admin')
+    ->namespace('Admin')
+    ->middleware(['auth','admin'])
+    ->group(function(){
+        Route::get('/', 'DashboardController@index')->name('admin-dashboard');
+        Route::resource('category','CategoryController');
+        Route::resource('user','UserController');
+        Route::resource('products','ProductController');
+        Route::resource('product-gallery','ProductGalleryController');
+    });
+
+Auth::routes();
+
+
